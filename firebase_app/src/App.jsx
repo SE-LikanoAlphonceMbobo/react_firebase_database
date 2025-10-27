b954a8d9978bead1d612af3af72513e048601d14
Likano <mbobolikano@gmail.com>
2025-10-27 22:29:19 +0200

    Adding routing

--- firebase_app/src/App.jsx
+++ firebase_app/src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Write from "./components/Write";
+  
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-        Fewfwefwe
+    <div className='card'>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Write/>} />
+            <Route path="/write" element={<Write/> }/>
+          </Routes>
+        </Router>
     </div>
   
-  )
+  );
 }
 
 export default App
---
+++ firebase_app/src/components/Write.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Write() {
+  return (
+    <div className='card'>
+      Write
+    </div>
+  )
+}
+
+export default Write
